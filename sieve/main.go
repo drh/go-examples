addf286841a3a2e7378688ad0a01105519cb35da
David R. Hanson <drh@drhanson.net>
2024-08-02 09:46:04 -0600

    Add sieve. Rename main packages to main.go to conform to idiomatic module layout/naming.

---
+++ sieve/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	n := 10
+	switch len(os.Args) {
+	case 2:
+		k, err := strconv.Atoi(os.Args[1])
+		if err != nil || k < 1 {
+			fmt.Printf("%v is an invalid argument; it must be a integer > 1\n", os.Args[1])
+			os.Exit(1)
+		}
+		n = k
+	case 1: // use default of 10
+	default:
+		fmt.Printf("too many arguments\n")
+		os.Exit(1)
+	}
+	ch := make(chan int, 1)
+	go source(ch)
+	for i := 0; i < n; i++ {
+		prime := <-ch
+		fmt.Print(" ", prime)
+		out := make(chan int, 1)
+		go filter(prime, ch, out)
+		ch = out
+	}
+	fmt.Println()
+}
+
+func source(out chan<- int) {
+	out <- 2
+	for i := 3; ; i += 2 {
+		out <- i
+	}
+}
+
+func filter(prime int, in <-chan int, out chan<- int) {
+	for {
+		n := <-in
+		if n%prime != 0 {
+			out <- n
+		}
+	}
+}
